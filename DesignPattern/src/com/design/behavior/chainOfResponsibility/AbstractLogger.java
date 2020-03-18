53512fd2549d92e0c3f9d1398ff6030abd3694f0
ButchQiuQiu <133465202@qq.com>
2020-03-18 11:46:19 +0800

    删除并过滤

--- DesignPattern/src/com/design/behavior/chainOfResponsibility/AbstractLogger.java
+++ DesignPattern/src/com/design/behavior/chainOfResponsibility/AbstractLogger.java
@@ -2,7 +2,7 @@ package com.design.behavior.chainOfResponsibility;
 
 public abstract class AbstractLogger {
 	public static int INFO=1;
-	public static int DEBUG=2;
+	public static int DEBUG=2; 
 	public static int ERROR=3;
 	protected int level;
 	protected AbstractLogger nextLogger;
